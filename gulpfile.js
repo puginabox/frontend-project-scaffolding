56c297cb94b0b86faee63b0e109e373cff683378
Griffin Byron <puginabox@gmail.com>
2015-02-28 10:31:02 -0500

    adding messaging logging

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
-var gulp = require('gulp);
+var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var jscs = require('gulp-jscs');
-                   
+var util = require('gulp-util');       
                    
                    
                    
@@ -9,13 +9,32 @@ var jscs = require('gulp-jscs');
                    
                    
 gulp.task('vetting', function() {
-   return gulp
-    .src('[
+    log('Analyizing source with JSHint & JSCS');
+    return gulp
+    .src([
         './src/**/*.js',
              './*.js'
-    ]')
+    ])
     .pipe(jscs())
     .pipe(jshint(jshint.reporter('jshint-stylish', { verbose: true })));
-    
-    
 });
+
+
+
+
+
+
+
+////////////////////
+function log(msg){
+
+    if(typeof(msg) === 'object') {
+        for (var item in msg) {
+            if (msg.hasOwnProperty(item)) {
+                util.log(util.colors.blue(msg[item]));
+            }
+        }
+    } else {
+        util.log(util.colors.blue(msg));
+    }
+}
